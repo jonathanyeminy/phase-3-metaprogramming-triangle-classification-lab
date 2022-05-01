fe90b47a37e1d1caa92b7650ab0e361da445eda7
Jonathan Yeminy <jonathanyeminy@yahoo.com>
2022-05-01 19:04:42 -0400

    commit tree df209ec1556d2fee399c52c5d6b1c027151b1bd3

--- lib/triangle.rb
+++ lib/triangle.rb
@@ -1,3 +1,32 @@
 class Triangle
-  # write code here
+  attr_accessor :side1, :side2, :side3
+
+  def initialize(side1, side2, side3)
+    @side1 = side1
+    @side2 = side2
+    @side3 = side3
+  end
+
+  def kind
+    triangle_is_valid
+    if (side1 == side2 && side1 == side3)
+      :equilateral
+    elsif (side1 != side2 && side1 != side3 && side2 != side3)
+      :scalene
+    else
+      :isosceles
+    end
+     
+  end
+  
+  def triangle_is_valid
+    raise TriangleError unless
+    (self.side1>0 && self.side2>0 && self.side3>0) &&
+    (side1+side2 > side3) &&
+    (side2+side3 > side1) &&
+    (side1+side3 > side2)
+  end
+
+  class TriangleError < StandardError
+  end
 end
